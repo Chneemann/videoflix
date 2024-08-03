fd21bbf9bc44f1ac0254648ebbab834cc3692649
Chneemann <Andre-Ke@gmx.de>
2024-08-03 17:42:32 +0200

    show the films in categories & last added, mark the selected one

--- frontend/src/app/components/home/browse/browse.component.ts
+++ frontend/src/app/components/home/browse/browse.component.ts
@@ -17,20 +17,23 @@ export class BrowseComponent implements OnInit {
       title: 'Our Galaxy',
       description:
         'Experience the fascinating journey through the infinite expanse of our galaxy. "Our Galaxy" offers a detailed insight into the breathtaking structures, stars and planets of our cosmic home. Through stunning visual effects and comprehensive explanations, this documentary guides viewers through the mysteries of the universe and reveals the beauty and complexity of outer space. Perfect for astronomy lovers and the curious who want to learn more about the wonders of the universe',
-      category: 'documentation',
+      category: 'Documentary',
       imgPath: 'galaxy.png',
       videoPath: 'galaxy.mp4',
+      create: '08.03.2024',
     },
     {
       id: 2,
       title: 'Star Trek',
       description:
         'Join Captain Kirk and the crew of the starship USS Enterprise as they embark on thrilling adventures across the universe. "Star Trek" is a legendary sci-fi saga that explores the final frontier, filled with action, exploration, and unforgettable encounters with alien species. This epic journey showcases the spirit of discovery and the unwavering courage of the Starfleet members as they confront the unknown and protect the galaxy from various threats.',
-      category: 'sci-fi',
+      category: 'Science Fiction',
       imgPath: 'star-trek.png',
       videoPath: 'star-trek.mp4',
+      create: '08.02.2024',
     },
   ];
+
   currentMovie: any[] = [];
 
   ngOnInit(): void {
--- frontend/src/app/components/home/browse/categories/categories.component.ts
+++ frontend/src/app/components/home/browse/categories/categories.component.ts
@@ -1,21 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss',
 })
 export class CategoriesComponent {
   @Input() movies: any[] = [];
+  @Input() currentMovie: number = 0;
   @Output() currentMovieId = new EventEmitter<number>();
 
+  filmGenres = [
+    'Action',
+    'Adventure',
+    'Comedy',
+    'Drama',
+    'Horror',
+    'Science Fiction',
+    'Fantasy',
+    'Romance',
+    'Thriller',
+    'Mystery',
+    'Crime',
+    'Animation',
+    'Documentary',
+    'Musical',
+    'War',
+    'Western',
+  ];
+
   openCurrentMovie(movieId: number) {
+    this.currentMovie = movieId;
     this.currentMovieId.emit(movieId);
   }
 
-  get documentaryMovies() {
-    return this.movies.filter((movie) => movie.category === 'documentary');
+  allMovies(filmGenre: string) {
+    return this.movies.filter((movie) => movie.category === filmGenre);
+  }
+
+  recentMovies() {
+    const today = new Date();
+    const sevenDaysAgo = new Date(today);
+    sevenDaysAgo.setDate(today.getDate() - 7);
+
+    return this.movies.filter((movie) => {
+      const movieDate = new Date(movie.create);
+      return movieDate >= sevenDaysAgo;
+    });
   }
 }
