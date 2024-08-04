94b096e6c059dcf82b1b4f2036d19ae4a718c4e9
Chneemann <Andre-Ke@gmx.de>
2024-08-04 10:02:07 +0200

    added user api-view

--- backend/videoflix/users/views.py
+++ backend/videoflix/users/views.py
@@ -1,3 +1,44 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import UserSerializer
+from .models import CustomUser
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def user_list(request):
+  
+  if request.method == 'GET':
+    user = CustomUser.objects.all()
+    serializer = UserSerializer(user, many=True)
+    return Response(serializer.data)
+
+  elif request.method == 'POST':
+    serializer = UserSerializer(data= request.data)
+    if serializer.is_valid():
+      serializer.save
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, id):
+  
+    try:
+      user = CustomUser.objects.get(pk=id)
+    except CustomUser.DoesNotExist:
+      return Response(status=status.HTTP_404_NOT_FOUND)
+  
+    if request.method == 'GET':
+      serializer = UserSerializer(user)
+      return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+      serializer = UserSerializer(user, data=request.data)
+      if serializer.is_valid():
+        serializer().save()
+        return Response(serializer.data)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+      user.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
--- backend/videoflix/videoflix/urls.py
+++ backend/videoflix/videoflix/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', views.user_list),
+    path('users/<int:id>/', views.user_detail),
 ]
