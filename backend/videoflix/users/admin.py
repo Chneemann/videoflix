f8af9ee5df7620090e78f3fe429adc67cf9aacea
Chneemann <Andre-Ke@gmx.de>
2024-08-04 08:57:10 +0200

    customized form added to the admin interface for the user

--- backend/videoflix/users/admin.py
+++ backend/videoflix/users/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from .models import CustomUser
+from .forms import CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):    
+  add_form = CustomUserCreationForm    
+  
+  fieldsets = (        
+    *UserAdmin.fieldsets,        
+    (            
+      'Individual data',            
+      {                
+        'fields': (                    
+          'custom',                    
+          'phone',                    
+          'address',                
+        )            
+      }        
+    )    
+  )
