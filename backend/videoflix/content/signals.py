5510054961a9eaa445f72117b0c2f0e81f76f99e
Chneemann <Andre-Ke@gmx.de>
2024-08-11 09:23:49 +0200

    add signal handler to auto-delete video file on Video model deletion

---
+++ backend/videoflix/content/signals.py
@@ -0,0 +1,14 @@
+from .models import Video
+from django.dispatch import receiver
+from django.db.models.signals import post_delete
+import os
+
+@receiver(post_delete, sender=Video)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `Video` object is deleted.
+    """
+    if instance.video_file:
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
