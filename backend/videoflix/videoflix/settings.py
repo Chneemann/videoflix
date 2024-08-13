2c7fd7a9ca213506275c514a8f6b4b8e3b1a1fff
Chneemann <Andre-Ke@gmx.de>
2024-08-13 16:20:28 +0200

    datenbank auf postgresql umgestellt

--- backend/videoflix/videoflix/settings.py
+++ backend/videoflix/videoflix/settings.py
@@ -130,10 +130,21 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'videoflix',
+        'USER': 'chneemann',
+        'PASSWORD': '91&yWMr6',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
