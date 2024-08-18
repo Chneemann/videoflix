c9f2d0a0541706f4d8f26693093b9853347c155f
Chneemann <Andre-Ke@gmx.de>
2024-08-18 21:06:46 +0200

    update environment for v-server

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   // Development
-  baseUrl: 'http://127.0.0.1:8000',
+  // baseUrl: 'http://127.0.0.1:8000',
 
   // Live
-  // baseUrl: 'https://videoflix-django.andre-kempf.com',
+  baseUrl: 'https://videoflix-django.andre-kempf.com',
 };
