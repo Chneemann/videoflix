7ba48b17d1656ed5ea61d0d28126845ff1cf6eb7
Chneemann <Andre-Ke@gmx.de>
2024-08-25 16:35:58 +0200

    added documentation

--- backend/videoflix/content/tests.py
+++ backend/videoflix/content/tests.py
@@ -1,14 +1,15 @@
 from django.test import TestCase
 from unittest.mock import patch, MagicMock
-from .tasks import convert_video_to_hls, delete_original_video, create_thumbnails
-from .signals import auto_delete_file_on_delete
-from .models import Video
+from .tasks import convert_video_to_hls, delete_original_video
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 class VideoTasksTest(TestCase):
     @patch('subprocess.run')
     def test_convert_video_to_hls(self, mock_subprocess_run):
+        """
+        Tests whether the HLS conversion script is called correctly and the correct parameters are passed.
+        """
         mock_subprocess_run.return_value = MagicMock()
         convert_video_to_hls('test/source.mp4', '1080', 1)
         mock_subprocess_run.assert_called_once()
@@ -17,5 +18,8 @@ class VideoTasksTest(TestCase):
 
     @patch('os.remove')
     def test_delete_original_video(self, mock_os_remove):
+        """
+        Checks whether the original video file is successfully deleted after conversion.
+        """
         delete_original_video('test/source.mp4')
         mock_os_remove.assert_called_once_with('test/source.mp4')
