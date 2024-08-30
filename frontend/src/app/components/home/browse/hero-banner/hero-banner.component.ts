da1a1b15160a9eff537c5944f357d45b69448be5
Chneemann <Andre-Ke@gmx.de>
2024-08-30 10:38:03 +0200

    bugfixes

--- frontend/src/app/components/home/browse/hero-banner/hero-banner.component.ts
+++ frontend/src/app/components/home/browse/hero-banner/hero-banner.component.ts
@@ -58,8 +58,6 @@ export class HeroBannerComponent implements OnChanges {
     }
   }
 
-  refreshPage(movieId: number) {}
-
   toggleLikeMovie(movieId: number): void {
     if (this.favoriteMovies.includes(movieId)) {
       this.favoriteMovies = this.favoriteMovies.filter((id) => id !== movieId);
--- frontend/src/app/services/movie.service.ts
+++ frontend/src/app/services/movie.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, lastValueFrom, Observable, of } from 'rxjs';
+import { BehaviorSubject, lastValueFrom, Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 
 @Injectable({
